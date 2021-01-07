5a3d5e55fe0770832988909392f3f6b5838b41c9
秋雨落 <i@rain.cx>
2021-01-07 09:10:12 +0800

    :sparkles: update fabric version, redo all events
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/fabric/raincommons/event/callback/PlayerLoginCallback.java
@@ -0,0 +1,20 @@
+package cx.rain.mc.fabric.raincommons.event.callback;
+
+import com.mojang.authlib.GameProfile;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.util.ActionResult;
+
+public interface PlayerLoginCallback {
+    Event<PlayerLoginCallback> EVENT = EventFactory.createArrayBacked(PlayerLoginCallback.class,
+            listeners -> (profile, connection) -> {
+                for (PlayerLoginCallback callback : listeners) {
+                    if (callback.accept(profile, connection) == ActionResult.FAIL) {
+                        return ActionResult.FAIL;
+                    }
+                }
+            });
+
+    ActionResult accept(GameProfile profile, ClientConnection connection);
+}
